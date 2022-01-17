6aa564abc1ee1e5a3a87bb314b636089a617f809
David Matas <adavidmatas@gmail.com>
2022-01-17 10:46:01 +0100

    Season 1

--- 01-outside-in-with-ohce-kata/src/ohce.js
+++ 01-outside-in-with-ohce-kata/src/ohce.js
@@ -1,3 +1,16 @@
-export default () => {
-  return false;
+export default (momentDay, output, input) => {
+  return {
+    run(name) {
+      if (momentDay.isNight()) output.run(`¡Buenas noches ${name}!`);
+      if (momentDay.isEvening()) output.run(`¡Buenas tardes ${name}!`);
+      if (momentDay.isMorning()) output.run(`¡Buenos días ${name}!`);
+
+      let currentInput = input.askInput();
+
+      while(currentInput !== 'Stop!') {
+        currentInput = input.askInput();
+      }
+      return output.run(`Adios ${name}`);
+    }
+  }
 }
--- 01-outside-in-with-ohce-kata/src/ohce.test.js
+++ 01-outside-in-with-ohce-kata/src/ohce.test.js
@@ -1,5 +1,56 @@
 import ohce from "./ohce";
 
-test("canary test", () => {
- expect(false).toEqual(true);
+it("greeting at night", () => {
+  const nightMoment = momentDay({isNight: true});
+  const outputMock = output();
+  const inputMock = input({onAskInput: 'Stop!'});
+  const app = ohce(nightMoment, outputMock, inputMock);
+  const name = 'Pedro';
+
+  app.run(name);
+
+ expect(outputMock.run).toHaveBeenCalledWith('¡Buenas noches Pedro!');
+ expect(outputMock.run).toHaveBeenCalledWith('Adios Pedro');
 });
+
+it.skip("greeting at evening", () => {
+  const eveningMoment = momentDay({isEvening: true});
+  const outputMock = output();
+  const app = ohce(eveningMoment, outputMock);
+  const name = 'Pedro';
+
+  app.run(name);
+
+ expect(outputMock.run).toHaveBeenCalledWith('¡Buenas tardes Pedro!');
+});
+
+it.skip("greeting at morning", () => {
+  const morningMoment = momentDay({isMorning: true});
+  const outputMock = output();
+  const app = ohce(morningMoment, outputMock);
+  const name = 'Pedro';
+
+  app.run(name);
+
+ expect(outputMock.run).toHaveBeenCalledWith('¡Buenos días Pedro!');
+});
+
+function output() {
+  return {
+    run: jest.fn()
+  }
+}
+
+function input(params){
+  return {
+    askInput: () => params.onAskInput
+  }
+}
+
+function momentDay(params) {
+  return {
+    isNight: () => params.isNight,
+    isEvening: () => params.isEvening,
+    isMorning: () => params.isMorning,
+  }
+}
